dd55981345584f21f67435b25177be593bf8d46e
N-Adkins <noahcadkins@gmail.com>
2024-04-09 22:33:28 -0400

    Added AST deinit

--- src/parser.zig
+++ src/parser.zig
@@ -7,12 +7,39 @@ pub const ParseError = error {
 
 pub const Expression = union(enum) {
     number_constant: i64,
+    function_decl: struct { args: std.ArrayList([]u8), body: Block },
     call: struct { callee: *Expression, args: std.ArrayList(Expression) },
+
+    pub fn deinit(self: *Expression, allocator: *std.mem.Allocator) void {
+        switch (self.*) {
+            .number_constant => {},
+            .function_decl => |func| {
+                for (func.args.items) |arg| {
+                    allocator.free(arg);
+                }
+                func.args.deinit();
+                func.body.deinit();
+            },
+            .call => |call| {
+                call.args.deinit();
+                call.callee.deinit(allocator);
+            }
+        }
+    }
 };
 
 pub const Statement = union(enum) {
     variable_decl: struct { name: []const u8, value: *Expression },
     block: Block,
+
+    pub fn deinit(self: *Statement, allocator: *std.mem.Allocator) void {
+        switch (self.*) {
+            .variable_decl => |decl| {
+                allocator.free(decl.name);
+                decl.value.deinit(allocator);
+            }
+        }
+    }
 };
 
 pub const Block = struct {
