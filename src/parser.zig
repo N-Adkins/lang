b519ec76c6c425db828c7a5e602ca9176467a884
N-Adkins <N-Adkins@github.com>
2024-04-10 08:23:13 -0500

    Basic parsing

--- src/parser.zig
+++ src/parser.zig
@@ -1,7 +1,10 @@
 const std = @import("std");
 const lexer = @import("lexer.zig");
 
-pub const ParseError = error{} || std.mem.Allocator.Error;
+pub const ParseError = error {
+    UnexpectedToken,
+    UnexpectedEnd,
+} || std.mem.Allocator.Error;
 
 pub const Expression = union(enum) {
     number_constant: i64,
@@ -50,10 +53,67 @@ pub const Block = struct {
     }
 
     pub fn deinit(self: *Block) void {
+        for (self.statements.items) |statement| {
+            statement.deinit();
+        }
         self.statements.deinit();
     }
 };
 
 pub const Parser = struct {
     lexer: *lexer.Lexer,
+    allocator: *std.mem.Allocator,
+    root: Block,
+    current: ?lexer.Token = null,
+    previous: ?lexer.Token = null,
+
+    pub fn init(lex: *lexer.Lexer, allocator: *std.mem.Allocator) Parser {
+        return Parser{
+            .lexer = lex,
+            .allocator = allocator,
+            .root = Block.init(allocator),
+        };
+    }
+
+    pub fn parse(self: *Parser) ParseError!void {
+        self.nextToken();
+        while (self.lexer.queue.len > 0) {
+            const statement = try parseStatement();
+            try self.root.statements.append(statement);
+        }
+    }
+
+    fn parseStatement(self: *Parser) ParseError!*Statement {
+        if (self.current == null) {
+            return ParseError.UnexpectedEnd;
+        }
+
+        const statement = switch (self.current.?.tag) {
+            .keyword_var => try self.parseStatement(),
+        };
+        errdefer statement.deinit();
+
+        _ = try self.expectToken(.semicolon);
+
+        return statement;
+    }
+
+    fn expectToken(self: *Parser, tag: lexer.TokenTag) ParseError!lexer.Token {
+        if (self.current) |current| {
+            if (current.tag == tag) {
+                self.nextToken();
+                return current;
+            } else {
+                return ParseError.UnexpectedToken;
+            }
+        } else {
+            return ParseError.UnexpectedToken;
+        }
+    }
+
+    fn nextToken(self: *Parser) lexer.Token {
+        self.previous = self.current;
+        self.current = self.lexer.nextToken();
+        return self.current;
+    }
 };
