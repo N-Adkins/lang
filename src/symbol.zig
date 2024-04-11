5e9fd86d232628d493bd81f0e7197a9304d1d633
N-Adkins <N-Adkins@github.com>
2024-04-11 14:01:24 -0500

    Work on symbol check pass

--- src/symbol.zig
+++ src/symbol.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
+const ast = @import("ast.zig");
 const types = @import("types.zig");
+
+pub const SymbolError = error{
+    SymbolNotFound,
+} || std.mem.Allocator.Error;
 
 pub const SymbolTable = struct {
     parent: ?*SymbolTable,
@@ -19,4 +24,51 @@ pub const SymbolTable = struct {
         }
         self.table.deinit();
     }
+
+    fn checkSymbolExists(self: *SymbolTable, symbol: []const u8) SymbolError!void {
+        if (self.table.get(symbol)) |_| {
+            return;
+        }
+
+        if (self.parent) |parent| {
+            try parent.checkSymbolExists(symbol);
+        }
+    }
+
+    fn getSymbol(self: *SymbolTable, symbol: []const u8) SymbolError!*types.Type {
+        if (self.table.get(symbol)) |*symbol_type| {
+            return symbol_type;
+        }
+
+        if (self.parent) |parent| {
+            return try parent.getSymbol(symbol);
+        }
+
+        return null;
+    }
+
+    fn setSymbol(self: *SymbolTable, symbol: []const u8, symbol_type: types.Type) SymbolError!void {
+        try self.table.put(symbol, symbol_type);
+    }
 };
+
+pub fn checkSymbols(root: *ast.Block) SymbolError!void {
+    return checkBlock(root); 
+}
+
+fn checkExpression(expr: *ast.Expression) SymbolError!void {
+
+}
+
+fn checkStatement(scope: *ast.Block, statement: *ast.Statement) SymbolError!void {
+    switch (statement) {
+        .block => |block| try checkBlock(block),
+        .variable_decl => |variable| {
+            
+        }
+    }
+}
+
+fn checkBlock(scope: ?*ast.Block, block: *ast.Block) SymbolError!void {
+
+}
