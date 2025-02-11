f5871e668ae089cb09eb49fa4793513368bf1d40
N-Adkins <noahcadkins@gmail.com>
2025-02-10 22:01:12 -0500

    init

--- build.zig
+++ build.zig
@@ -1,22 +1,29 @@
 const std = @import("std");
 
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "lang",
-        .root_source_file = .{ .path = "src/main.zig" },
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-        .use_llvm = true, 
-        .use_lld = true,
     });
 
+    const exe = b.addExecutable(.{
+        .name = "lang",
+        .root_module = exe_mod,
+        .use_llvm = false,
+        .use_lld = false,
+    });
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
+
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
@@ -25,31 +32,11 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-        .use_llvm = false,
-        .use_lld = false,
+        .root_module = exe_mod,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
-    
-    const docs = b.addObject(.{
-      .name = "Lang",
-      .root_source_file = .{ .path = "src/main.zig" },
-      .target = target,
-      .optimize = optimize,
-    });
-
-    const install_docs = b.addInstallDirectory(.{
-      .source_dir = docs.getEmittedDocs(),
-      .install_dir = .prefix,
-      .install_subdir = "docs",
-    });
-
-    const docs_step = b.step("docs", "Generate documentation");
-    docs_step.dependOn(&install_docs.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,39 +1,5 @@
 const std = @import("std");
-const byte = @import("runtime/bytecode.zig");
-const compiler = @import("compiler/compiler.zig");
-const runtime = @import("runtime/runtime.zig");
-
-test {
-    std.testing.refAllDeclsRecursive(@This());
-}
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    if (std.os.argv.len < 2) {
-        std.debug.print("Expected filepath\n", .{});
-        return;
-    }
-
-    const filepath: []const u8 = std.mem.span(std.os.argv[1]);
-
-    const file = std.fs.cwd().openFile(filepath, .{}) catch |err| {
-        std.debug.print("Failed to load file \"{s}\": {}\n", .{ filepath, err });
-        return;
-    };
-    defer file.close();
-
-    const reader = file.reader();
-    const str = try reader.readAllAlloc(allocator, 0xFFFF);
-    defer allocator.free(str);
-
-    var compile_result = compiler.compile(allocator, str) catch {
-        return;
-    };
-    defer compile_result.deinit(allocator);
-    byte.dumpBytecode(compile_result.bytecode);
-
-    runtime.run(allocator, compile_result.bytecode, compile_result.constants);
-}
+    std.debug.print("Test\n", .{});
+} 
